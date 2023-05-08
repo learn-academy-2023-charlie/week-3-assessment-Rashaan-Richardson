76a05d5bd81c648d364ce1d542893d5807912909
Rashaan-Richardson <rashaan.richardson@gmail.com>
2023-05-08 09:56:12 -0600

    Adding Week 3 assesment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,8 +12,22 @@
 
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
+// Pseudo code: 
+// input: a number that is how long you want to sequence to run for
+// output : an array of numbers that are the fibbo sequence 
+      // const fibLength1 = 6
+      // Expected output: [1, 1, 2, 3, 5, 8]
+// process: create inital array and push the new numbers to the end while indexing
+
 // a) Create a test with expect statements for each of the variables provided.
 
+// describe("fibSeq", () => {
+  //     it("Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.", () => {
+  //         const fibLength1 = 6
+  //         expect(titleCase(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+  //     })
+  // })
+  
 const fibLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
@@ -21,12 +35,58 @@ const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 // b) Create the function that makes the test pass.
+const fibSeq = (element) =>{
+  var fibbo = [1,1]
+  if (element > 2){
+    for (let i = 2; i < element; i++){
+      fibbo.push(fibbo[fibbo.length-1] + fibbo[fibbo.length-2])
+    }
+  }
+  return fibbo
+}
+console.log(fibSeq(fibLength1));
+console.log(fibSeq(fibLength2));
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
+
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
+
+// Pseudo code: 
+// input: an object with k,v pairs in which the v is a number
+// output : sorted array by the values in ascending order
+// const studyMinutesWeek1 = {
+//   sunday: 90,
+//   monday: 30,
+//   tuesday: 20,
+//   wednesday: 15,
+//   thursday: 30,
+//   friday: 15,
+//   saturday: 60
+// }
+// Expected output: [15, 15, 20, 30, 30, 60, 90]
+// process: use the Object.entries built in method 
 
 // a) Create a test with expect statements for each of the variables provided.
 
+// describe("sortObj", () => {
+  //     it("Create a function that takes in an object and returns an array of the values sorted from least to greatest.", () => {
+  //         const studyMinutesWeek1 = {
+//   sunday: 90,
+//   monday: 30,
+//   tuesday: 20,
+//   wednesday: 15,
+//   thursday: 30,
+//   friday: 15,
+//   saturday: 60
+// }      expect(sortObj(studyMinutesWeek1)).toEqual( [ 'wednesday', 15 ],
+  // [ 'friday', 15 ],
+  // [ 'tuesday', 20 ],
+  // [ 'monday', 30 ],
+  // [ 'thursday', 30 ],
+  // [ 'saturday', 60 ],
+  // [ 'sunday', 90 ])
+  //     })
+  // })
 const studyMinutesWeek1 = {
   sunday: 90,
   monday: 30,
@@ -50,10 +110,29 @@ const studyMinutesWeek2 = {
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
 // b) Create the function that makes the test pass.
+const sortObj = (element) => {
+  return Object.entries(element).sort(([,a],[,b]) => a- b)
+}
+console.log(sortObj(studyMinutesWeek1))
+console.log(sortObj(studyMinutesWeek2))
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
+// Pseudo code: 
+// input: an array of numbers
+// output : an array of numbers that are the sum of all the values before it in the array 
+// const accountTransactions1 = [100, -17, -23, -9]
+// Expected output: [100, 83, 60, 51]
+// process: create a variable and update it as we iterate over the array 
+
 // a) Create a test with expect statements for each of the variables provided.
+
+// describe("accSum", () => {
+  //     it("Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.", () => {
+    // const accountTransactions1 = [100, -17, -23, -9]
+  //         expect(accSum( accountTransactions1 )).toEqual([100, 83, 60, 51])
+  //     })
+  // })
 
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
@@ -65,3 +144,11 @@ const accountTransactions3 = []
 // Expected output: []
 
 // b) Create the function that makes the test pass.
+const accSum = (element) => {
+  tot = 0
+  return element.map((value) => tot += value)
+}
+console.log(accSum(accountTransactions1));
+console.log(accSum(accountTransactions2));
+console.log(accSum(accountTransactions3));
+
--- interview-questions.md
+++ interview-questions.md
@@ -9,47 +9,76 @@ Then, research the question to expand on your answer. Even if you feel you have 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
 Your answer:
+JSX stands for JavaScript syntax extension. JSX allows us to use React syntax that is similar to HTML. A huge difference is you must return one element or it wont complie. Which is why we use '<></>'
 
 Researched answer:
 
-1. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
+JSX stands for JavaScript syntax extension. It is a JavaScript extension that allows us to describe React's object tree using a syntax that resembles that of an HTML template. It is just an XML-like extension that allows us to write JavaScript that looks like markup and have it returned from a component.
+
+2. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
 Your answer:
 
+Yarn is a package manager for Node. js. After running it in your terminal, you can see additional packages in the directory. As well as run react on your browser.
+
 Researched answer:
+
+The yarn install command is a command that is used to install all dependencies for a project. It is commonly used when you have just checked out code for a project, or when you need to pick up a dependency that another developer on your project has added to the project.
 
 3. What is a React component?
 
 Your answer:
 
+Component are pieces of code that is pushed to the App.js file. Mainly for two reasons. Makes the code neat and organized. As well as easy to debug if an issue arises 
+
 Researched answer:
+
+Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML. Components come in two types, Class components and Function components
 
 4. What is the difference between state values and props in React?
 
 Your answer:
+Props are independant of external factors while states are called and within the component 
 
 Researched answer:
+
+The key difference between props and state is that state is internal and controlled by the component itself while props are external and controlled by whatever renders the component.
 
 5. What is the DOM?
 
 Your answer:
 
+It is the platform that updates your webpage with the updated info or changes in realtime 
+
 Researched answer:
+ Document Object Model (DOM) is a platform and language-neutral interface that allows programs and scripts to dynamically access and update the content, structure, and style of a document."
 
 6. STRETCH: Which is the difference between a div tag and a span tag?
 
 Your answer:
+Div creates a new element while span encompasses the original 
 
 Researched answer:
+a div tag creates a block-level element while a <span> tag wraps around an inline element.
 
 ## Looking Ahead: Terms for Next Week
 
 1. Object-oriented programming:
 
+Object-oriented programming (OOP) is a computer programming model that organizes software design around data, or objects, rather than functions and logic. An object can be defined as a data field that has unique attributes and behavior.
+
 2. Ruby:
+
+Ruby is mainly used to build web applications and is useful for other programming projects. It is widely used for building servers and data processing, web scraping, and crawling. The leading framework used to run Ruby is Ruby on Rails, although that's not the only one.
 
 3. Implicit return:
 
+If a JavaScript arrow function immediately returns a value, writing return is optional (if we also remove the curly brackets). The return is then implied, making it an “implicit return”.
+
 4. Ruby blocks:
 
+Ruby blocks are little anonymous functions that can be passed into methods. Blocks are enclosed in a do / end statement or between brackets {} , and they can have multiple arguments. The argument names are defined between two pipe | characters.
+
 5. Ruby hashes:
+
+A Hash is a collection of key-value pairs like this: "employee" = > "salary". It is similar to an Array, except that indexing is done via arbitrary keys of any object type, not an integer index
